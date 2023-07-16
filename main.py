b8c144f54b190188b2e3462e83634da774e70c29
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2023-07-16 20:29:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import pickle
 import pandas as pd
 import streamlit as st
+from keras.models import load_model
+import cv2
+import json
+from PIL import Image
+import numpy as np
 
 with open('diagnosis_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
+skin_model = load_model('skin_diseases.h5', compile=True)
+file = open('dat.json')
+skin_data = json.load(file)
+skin_data_keys = list(skin_data)
 data = pd.read_csv('data/Training.csv')
 data = pd.DataFrame(data)
 data.drop('Unnamed: 133', axis=1, inplace=True)
 data.drop('prognosis', axis=1, inplace=True)
 
+
+def get_skin_predictions(image):
+    img = cv2.resize(image, (32,32))/float(255)
+    prediction = skin_model.predict(img.reshape(1,32,32,3))
+    ai_choice = skin_data_keys[prediction.argmax()]
+    return ai_choice, skin_data[ai_choice]
 
 def get_predictions(symptoms):
     reconstructed_symptoms = [1 if symptom in symptoms else 0 for symptom in data.columns]
@@ -43,43 +58,77 @@ def generate_desc(dgs):
 
 st.markdown("<h1 style='text-align: center;'>Pocket Dokta 👨🏾‍⚕️</h1>", unsafe_allow_html=True)
 
-dokta = st.chat_message('assistant')
+text, image  = st.tabs(['General Diagnosis', 'Skin Disease Detection'])
 
-with st.container():
-    dokta.write("Hello there👋🏾, "
-                "my name is **Pocket Dokta 👨🏾‍⚕️** and I am here to assist you with preliminary diagnosis.")
-    dokta.info('To optimize the nature of my feedback and accuracy of diagnosis, I have provided you a **list of '
-               'symptoms you can select from** based on what you are feeling.')
+with text:
+    dokta = st.chat_message('assistant')
 
-    user = st.chat_message("user")
-    user.write('**Symptoms**')
-    all_symptoms = sorted(list(data.columns))
-    user_symptoms = user.multiselect('Feel free to select as many symptoms based on how you feel',
-                                   options=all_symptoms, key='stp')
+    with st.container():
+        dokta.write("Hello there👋🏾, "
+                    "my name is **Pocket Dokta 👨🏾‍⚕️** and I am here to assist you with preliminary diagnosis.")
+        dokta.info('To optimize the nature of my feedback and accuracy of diagnosis, I have provided you a **list of '
+                'symptoms you can select from** based on what you are feeling.')
 
-    diagnose = user.button('Diagnose me')
+        user = st.chat_message("user")
+        user.write('**Symptoms**')
+        all_symptoms = sorted(list(data.columns))
+        user_symptoms = user.multiselect('Feel free to select as many symptoms based on how you feel',
+                                    options=all_symptoms, key='stp')
 
-    if diagnose and user_symptoms:
-        dokta_d1 = st.chat_message('assistant')
-        # dokta_d1.write('I understand that experiencing symptoms can be distressing and uncomfortable.🤗🫂')
+        diagnose = user.button('Diagnose me')
 
-        dokta_d1.success('The predictions provided are based on research data and may have varying levels of accuracy. '
-                         'However, it is crucial to consult a doctor or visit the nearest hospital for confirmation and '
-                         'appropriate treatment. Their expertise is essential in providing accurate diagnosis and '
-                         'personalized care for your specific situation.')
+        if diagnose and user_symptoms:
+            dokta_d1 = st.chat_message('assistant')
+            # dokta_d1.write('I understand that experiencing symptoms can be distressing and uncomfortable.🤗🫂')
 
-        labels, percentages = get_predictions(user_symptoms)
-        try:
-            descriptions = generate_desc(labels)
-        except:
-            descriptions = ['','','']
-        r, l, g = dokta_d1.columns(3)
-        with r:
-            st.metric('First possible diagnosis', f'{labels[2]}', f'{percentages[1]}')
-            st.write(descriptions[2])
-        with l:
-            st.metric('Second possible diagnosis', f'{labels[0]}', f'{percentages[0]}')
-            st.write(descriptions[0])
-        with g:
-             g.metric('Third Possible diagnosis', f'{labels[1]}', f'{percentages[2]}')
-             st.write(descriptions[1])
+            dokta_d1.success('The predictions provided are based on research data and may have varying levels of accuracy. '
+                            'However, it is crucial to consult a doctor or visit the nearest hospital for confirmation and '
+                            'appropriate treatment. Their expertise is essential in providing accurate diagnosis and '
+                            'personalized care for your specific situation.')
+
+            labels, percentages = get_predictions(user_symptoms)
+            try:
+                descriptions = generate_desc(labels)
+            except:
+                descriptions = ['','','']
+            r, l, g = dokta_d1.columns(3)
+            with r:
+                st.metric('First possible diagnosis', f'{labels[2]}', f'{percentages[1]}')
+                st.write(descriptions[2])
+            with l:
+                st.metric('Second possible diagnosis', f'{labels[0]}', f'{percentages[0]}')
+                st.write(descriptions[0])
+            with g:
+                g.metric('Third Possible diagnosis', f'{labels[1]}', f'{percentages[2]}')
+                st.write(descriptions[1])
+
+with image:
+    img_array = None
+    check = st.selectbox('Choose you mode of input', ['','Camera Capture', 'Image Upload'])
+    if check == 'Camera Capture':
+        with st.expander('Click here to take picture'):
+            capture = st.camera_input('Take a picture of the infected body part')
+            if capture:
+                img: Image = Image.open(capture)
+                img_array = np.array(img)
+    elif check == 'Image Upload':
+        upload = st.file_uploader('Upload Image here')
+        if upload:
+            img: Image = Image.open(upload)
+            img_array = np.array(img)
+    else:
+        st.warning('Please select an input mode')
+    try:
+        results = get_skin_predictions(image=img_array)
+        if results:
+            st.success('Image processed successfully ✅')
+            st.markdown('<hr>', unsafe_allow_html=True)
+            st.title('Results')
+            st.text_input('Diagnosis', value=results[0])
+            st.text_area('Description', value=results[1]['description'])
+            st.text_input('Causes', value=results[1]['causes'])
+            l, r = st.columns(2)
+            l.text_input('Possible treatment 1', value=results[1]['treatement-1'])
+            r.text_input('Possible treatment 2', value=results[1]['treatement-2'])
+    except:
+        pass
