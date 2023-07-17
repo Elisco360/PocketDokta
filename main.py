266d5e3fbff24859c9f21f72f5bd57cf8bcdaf85
Elijah Boateng <64059839+Elisco360@users.noreply.github.com>
2023-07-17 10:14:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,11 @@ import cv2
 import json
 from PIL import Image
 import numpy as np
-import urllib.request
-import os
-import tensorflow
 
 with open('diagnosis_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-@st.experimental_singleton
-def load_my_model():
-    if not os.path.isfile('model.h5'):
-        urllib.request.urlretrieve('https://github.com/Elisco360/PocketDokta/blob/main/skin_diseases.h5', 'model.h5')
-    return tensorflow.keras.models.load_model('model.h5')
-skin_model = load_my_model()
+skin_model = load_model('skin_diseases.h5', compile=True)
 file = open('dat.json')
 skin_data = json.load(file)
 skin_data_keys = list(skin_data)
